0349460c37e6fd501384727b48d9c771836c1589
Dr4K4n <stefan@dr4k4n.de>
2014-02-09 21:27:24 +0100

    Bugfix for the arquillian test.

--- survey-ejb/src/main/java/com/prodyna/ted/survey/persistence/PersistenceInterceptor.java
+++ survey-ejb/src/main/java/com/prodyna/ted/survey/persistence/PersistenceInterceptor.java
@@ -11,25 +11,25 @@ import org.slf4j.LoggerFactory;
  * Interceptor for logging service calls.
  * 
  * @author Daniel Knipping, PRODYNA AG
- *
  */
 @Interceptor
 @Logging
 public class PersistenceInterceptor {
 
-	private static final Logger LOG = LoggerFactory.getLogger(PersistenceInterceptor.class);
-	
-	@AroundInvoke
-	public Object doLogging(InvocationContext context) throws Exception {
-		Object proceed = null;
-		try {
-			long beforeMethodCall = System.currentTimeMillis();
-			proceed = context.proceed();
-			long afterMethodCall = System.currentTimeMillis();
-			LOG.info("Calling Service " + context.getMethod().getName() + "  " + (afterMethodCall - beforeMethodCall) + " ms");
-		} catch (Exception e) {
-			LOG.error("Error during service call", e);
-		}
-		return proceed;
-	}
+    private static final Logger LOG = LoggerFactory.getLogger(PersistenceInterceptor.class);
+
+    @AroundInvoke
+    public Object doLogging(InvocationContext context) throws Exception {
+        Object proceed = null;
+        try {
+            long beforeMethodCall = System.currentTimeMillis();
+            proceed = context.proceed();
+            long afterMethodCall = System.currentTimeMillis();
+            LOG.info("Calling Service " + context.getMethod().getName() + "  " + (afterMethodCall - beforeMethodCall) + " ms");
+        } catch (Exception e) {
+            LOG.error("Error during service call", e);
+            throw e;
+        }
+        return proceed;
+    }
 }
