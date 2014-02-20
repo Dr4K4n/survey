599a6b645da70397f153d4c6c9ca9068dd96b4cc
dknipping <daniel.knipping@prodyna.com>
2014-02-20 23:47:21 +0100

    Testfall repariert

--- survey-web/src/test/java/com/prodyna/ted/survey/CreateSurveyPageTest.java
+++ survey-web/src/test/java/com/prodyna/ted/survey/CreateSurveyPageTest.java
@@ -1,5 +1,6 @@
 package com.prodyna.ted.survey;
 
+import org.apache.wicket.bean.validation.BeanValidationConfiguration;
 import org.apache.wicket.mock.MockApplication;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Test;
@@ -10,7 +11,13 @@ public class CreateSurveyPageTest {
 
 	@Test
 	public void testSurveyPage() {
-		WicketTester tester = new WicketTester(new MockApplication());
+		WicketTester tester = new WicketTester(new MockApplication() {
+			@Override
+			protected void init() {
+				super.init();
+				new BeanValidationConfiguration().configure(this);
+			}
+		});
 		tester.startPage(new CreateSurveyPage());
 	}
 }
